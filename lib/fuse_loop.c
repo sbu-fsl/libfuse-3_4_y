1ef43b67bbc1b3c11ffa066104afdd8cf20b19e7
Sagar Jeevan <sjeevan@cs.stonybrook.edu>
2019-12-16 17:00:02 -0500

    fuse_loop.c: timed libfuse requests' fetch and processing time

--- lib/fuse_loop.c
+++ lib/fuse_loop.c
@@ -15,14 +15,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
 
 int fuse_session_loop(struct fuse_session *se)
 {
-	int res = 0;
+	int res = 0, fd;
+	ssize_t ret = 0;
+	double total_time = 0;
+	clock_t begin = 0, end;
+	char str[20];
 	struct fuse_buf fbuf = {
 		.mem = NULL,
 	};
 
+	begin = clock();
 	while (!fuse_session_exited(se)) {
 		res = fuse_session_receive_buf_int(se, &fbuf, NULL);
 
@@ -33,6 +43,8 @@ int fuse_session_loop(struct fuse_session *se)
 
 		fuse_session_process_buf_int(se, &fbuf, NULL);
 	}
+	end = clock();
+	total_time = (double)(end - begin) / CLOCKS_PER_SEC;
 
 	free(fbuf.mem);
 	if(res > 0)
@@ -42,5 +54,16 @@ int fuse_session_loop(struct fuse_session *se)
 	if(se->error != 0)
 		res = se->error;
 	fuse_session_reset(se);
+
+	fd = open("/root/new_clone/libfuse-3_4_y/lib/run_time.out", O_APPEND | O_WRONLY);
+	snprintf(str, 20, "%lf", total_time);
+	ret = pwrite(fd, str, strlen(str), 0);
+	
+	if (ret < 0)
+		printf("write to result file failed\n");
+	if (ret < strlen(str))
+		printf("write to result file partially failed\n");
+	close(fd);
+
 	return res;
 }
